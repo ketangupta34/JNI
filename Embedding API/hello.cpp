a92aef0b37af803da43cd00137674c9600f15203
Ketan Gupta <ketangupta34@gmail.com>
2021-04-11 15:44:33 +0530

    Delete Embedding API directory

--- Embedding API/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <iostream>
-#include "HelloWorldJNI.h"
-
-using namespace std;
-
-JNIEXPORT void JNICALL Java_HelloWorldJNI_sayHello(JNIEnv *env, jobject thisObject){
-  cout << "Hello from C++ !!" << endl;
-}
