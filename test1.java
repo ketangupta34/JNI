dab73b190d92476201d4f4ecab16f368261b6594
ketangupta34 <ketangupta34@gmail.com>
2021-04-18 16:31:02 +0530

    JavaCompiler API added to bootstrap API

---
+++ test1.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class test1 {
+   public static void printMessage() {
+      System.out.println("HELLO from test 1 prin function");
+   }
+
+   public static void main(String[] args) {
+      System.out.println("HELLO from Test 1 main function");
+   }
+}
